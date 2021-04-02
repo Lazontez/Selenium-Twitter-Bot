b90a2e5b44e6ae68e23d282fb549085b2050d237
Tez, Gardner <tezgardner1212@gmail.com>
2021-04-01 20:22:07 -0500

    Script is able to sign in to twitter and submit a post

---
+++ services/postOnTwitter.js
@@ -0,0 +1,98 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+require('dotenv').config()
+const chrome = require("selenium-webdriver/chrome");
+
+// WILL NEED TO ADD SIGN IN CREDENTIALS
+const signInData = {
+    'username': process.env.TWITTER_USER,
+    'password': process.env.TWITTER_PASSWORD
+
+}
+// Twitter Does Not Let You Post A Message That Contains THE SAME VALUE AS SOMETHING YOU HAVE PREVIOUSLY POSTED
+const details = {
+    'message': 'yamaha'
+}
+// NPM SELENIUM-WEBDRIVER OPTION SETTINS
+const options = new chrome.Options({ args: ['--window-size=1280,800', '--auto-open-devtools-for-tabs'] })
+
+// SETTING UP MEMORY FOR WEBDRIVERS PROXY *OPTIONAL(NOT NEEDED FOR CURRENT FUNCTIONALITY OF SCRIPT)
+// const proxy = require("selenium-webdriver/proxy");
+
+
+// FUNCTION TO SIGN IN TO TWITTER - TAKE A PARAM THAT EXPECTS WEB DRIVER INSTANCE TO BE PASSED THROUGH IT
+signInTwitter = (driver) => {
+
+    // WAITS UNTIL 'USERNAME' INPUT FIELD IS FOUND
+    driver.wait(until.elementLocated(By.name("session[username_or_email]"))).then(res => {
+        try {
+            console.log("Starting to fill out log-in page")
+            driver.findElement(By.name("session[username_or_email]")).sendKeys(signInData.username)
+            driver.findElement(By.name("session[password]")).sendKeys(signInData.password)
+            driver.findElement(By.xpath('/html/body/div/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div')).sendKeys(Key.ENTER)
+        }
+    // **NEED TO ADD CATCH VALIDATIONS
+        catch {
+            console.log('Caught an error')
+        }
+        finally {
+            console.log('Done')
+        }
+
+    })
+}
+
+// FUNCTION THAT WILL MAKE A POST TO TWITTER(SHOULD ALREADY BE SIGNED IN BEFORE RUNNING THIS)
+makePost = (driver) => {
+
+    // XPATH OF THE POST FIELD & POSTBTN
+    const xPathOfPostField = '/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div/span/br'
+    const xPathOfPostBtn = '/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]'
+    // WAITS UNTIL THE POST FIELD IS LOCATED ON THE PAGE
+    driver.wait(until.elementLocated(By.className('public-DraftStyleDefault-block'))).then(response => {
+        console.log('HI')
+        try {
+            // FINDS THE POST FIELD AND ENTERS THE DETAILS.MESSAGE VARIABLE
+            driver.findElement(By.xpath(xPathOfPostField)).sendKeys(details.message)
+            // WAITS UNTIL THE POST BUTTON IS FOUND(MEANING IT IS ENABLE) AND CLICKS BUTTON
+            driver.wait(until.elementLocated(By.xpath(xPathOfPostBtn))).then(response => {
+                console.log('Post button has been found')
+                driver.findElement(By.xpath(xPathOfPostBtn)).sendKeys(Key.ENTER).then(res => {
+                    console.log('Post has been made')
+                })
+            // NEED TO ADD MORE CATCH VALIDATIONS
+                .catch(err=>{
+                    console.log("Something went wrong when posting")
+                })
+            })
+        }
+        catch {
+            console.log('Something went wrong')
+        }
+        finally {
+            console.log('Finally')
+        }
+    })
+
+
+}
+
+// ASYNC FUNCTION THAT SETS UP WEB DRIVER INSTANCE AND CALLS ACTIONS
+async function postToTwitter() {
+
+    const driver = new Builder()
+        .setChromeOptions({ options: options })
+        .forBrowser("chrome").build();
+
+    await driver.get("https://twitter.com/login");
+
+    await signInTwitter(driver);
+    await makePost(driver)
+
+    // THIS FUNCTION IS ONLY SO THAT THE BROWSER DOES NOT CLOSE *NEED TO FIND A WAY TO MAKE BROWSER STAY OPEN WITHOUT THE USE OF THIS
+    driver.wait(until.elementLocated(By.name('xPathOfPostBtn')))
+
+
+
+}
+
+postToTwitter()
